c3b49cb32ff851e53332cfd6324523fb20321e1b
Okabintaro <103938900+Okabintaro@users.noreply.github.com>
2022-08-28 00:14:27 +0200

    dictparser: implement tuple reading
    Now everything should be set for paring the npy header. The only thing
    left ist parsing the descr field which describes the datatype.

--- src/dictparse.zig
+++ src/dictparse.zig
@@ -1,4 +1,3 @@
-const example = "{'descr': '<f8', 'fortran_order': False, 'shape': (99,), }";
 const std = @import("std");
 const mem = std.mem;
 
@@ -23,6 +22,9 @@ const Parser = struct {
         TrueLiteral2,
         TrueLiteral3,
 
+        TupleVal,
+        TupleNum,
+
         Finished,
     };
 
@@ -32,17 +34,21 @@ const Parser = struct {
         ExpectedKey,
         ExpectedTrue,
         ExpectedFalse,
+        ExpectedTupleNumber,
+        TupleTooBig,
     };
 
     pub const Value = union(enum) {
         boolean: bool,
         string: []const u8,
-        tuple: [0:MAX_DIM]u32,
+        tuple: [MAX_DIM:0]u32,
     };
 
     state: ParserState = ParserState.Start,
     start: usize = 0,
     key: ?[]const u8 = null,
+    tuple: Value = Value{ .tuple = .{0} ** MAX_DIM },
+    tupleIdx: usize = 0,
 
     pub fn init() Parser {
         return Parser{
@@ -117,11 +123,64 @@ const Parser = struct {
                         'F' => {
                             p.state = .FalseLiteral1;
                         },
+                        '(' => {
+                            p.state = .TupleVal;
+                            p.tupleIdx = 0;
+                        },
                         else => {
                             return ParseError.ExpectedColon;
                         },
                     }
                 },
+                // Tuple
+                .TupleVal => { // Expect Number
+                    switch (ch) {
+                        '1'...'9' => {
+                            p.start = i;
+                            p.state = .TupleNum;
+                        },
+                        ')' => {
+                            try map.put(p.key.?, p.tuple);
+                            p.tuple.tuple = .{0} ** p.tuple.tuple.len;
+                            p.state = .ExpectKey;
+                        },
+                        else => {
+                            return ParseError.ExpectedTupleNumber;
+                        },
+                    }
+                },
+                .TupleNum => { // Expect Number
+                    switch (ch) {
+                        '0'...'9' => {
+                            p.state = .TupleNum;
+                        },
+                        ',' => {
+                            if (p.tupleIdx >= p.tuple.tuple.len) {
+                                return ParseError.TupleTooBig;
+                            }
+                            const num_str = input[p.start..i];
+                            p.tuple.tuple[p.tupleIdx] = try std.fmt.parseUnsigned(u32, num_str, 10);
+                            p.tupleIdx += 1;
+                            p.state = .TupleVal;
+                        },
+                        ')' => {
+                            if (p.tupleIdx >= p.tuple.tuple.len) {
+                                return ParseError.TupleTooBig;
+                            }
+                            const num_str = input[p.start..i];
+                            p.tuple.tuple[p.tupleIdx] = try std.fmt.parseUnsigned(u32, num_str, 10);
+                            p.tupleIdx += 1;
+                            p.state = .ExpectKey;
+
+                            try map.put(p.key.?, p.tuple);
+                            p.tuple.tuple = .{0} ** p.tuple.tuple.len;
+                        },
+                        else => {
+                            return ParseError.ExpectedTupleNumber;
+                        },
+                    }
+                },
+
                 // True
                 .TrueLiteral1 => { // r -> u
                     switch (ch) {
@@ -311,5 +370,49 @@ test "multiple values mixed" {
         const val = result.get("ma_str").?;
         const expected = Parser.Value{ .string = "is_nice" };
         try testing.expectEqualStrings(expected.string, val.string);
+    }
+}
+
+test "single tuple" {
+    const in = "{'shape': (42,69,777)}";
+    const alloc = std.testing.allocator;
+    var parser = Parser.init();
+    var result = try parser.parseDict(in[0..], alloc);
+    defer result.deinit();
+    try testing.expectEqual(@intCast(u32, 1), result.count());
+    {
+        const val = result.get("shape").?;
+        const expected = Parser.Value{ .tuple = [8:0]u32{ 42, 69, 777, 0, 0, 0, 0, 0 } };
+        try testing.expectEqual(expected, val);
+    }
+}
+
+test "too big tuple" {
+    const in = "{'shape': (42,69,777,1,2,3,4,5,6)}";
+    const alloc = std.testing.allocator;
+    var parser = Parser.init();
+    var result = parser.parseDict(in[0..], alloc);
+    try testing.expectError(Parser.ParseError.TupleTooBig, result);
+}
+
+test "actual npy example" {
+    const example = "{'descr': '<f8', 'fortran_order': False, 'shape': (99), }";
+    const alloc = std.testing.allocator;
+    var parser = Parser.init();
+    var result = try parser.parseDict(example[0..], alloc);
+    defer result.deinit();
+    try testing.expectEqual(@intCast(u32, 3), result.count());
+    {
+        const val = result.get("descr").?;
+        try testing.expectEqualStrings("<f8", val.string);
+    }
+    {
+        const val = result.get("fortran_order").?;
+        try testing.expectEqual(false, val.boolean);
+    }
+    {
+        const val = result.get("shape").?;
+        const expected = Parser.Value{ .tuple = [8:0]u32{ 99, 0, 0, 0, 0, 0, 0, 0 } };
+        try testing.expectEqual(expected, val);
     }
 }
