34f13a95afac87f480395e04496c314809ba0b77
Okabintaro <103938900+Okabintaro@users.noreply.github.com>
2022-08-27 23:06:24 +0200

    dictparser: string and boolean parsing

--- src/dictparse.zig
+++ src/dictparse.zig
@@ -2,118 +2,314 @@ const example = "{'descr': '<f8', 'fortran_order': False, 'shape': (99,), }";
 const std = @import("std");
 const mem = std.mem;
 
-const ParserState = enum {
-    Start,
-    ExpectKey,
-    InKey,
-    ExpectColon,
-    InVal,
-    Finished,
-};
+const MAX_DIM = 8;
 
-const ParseError = error{
-    InvalidStart,
-    ExpectedColon,
-    Todo,
-};
+const Parser = struct {
+    pub const ParserState = enum {
+        Start,
+        ExpectKey,
+        InKey,
+        ExpectColon,
+        ExpectVal,
 
-fn parseDict(input: []const u8, allocator: mem.Allocator) anyerror!std.StringHashMap([]const u8) {
-    _ = input;
-    var map = std.StringHashMap([]const u8).init(allocator);
-    errdefer map.deinit();
+        StringVal,
 
-    var state: ParserState = ParserState.Start;
-    var start: usize = 0;
-    var end: usize = 0;
-    var key: ?[]const u8 = null;
+        FalseLiteral1,
+        FalseLiteral2,
+        FalseLiteral3,
+        FalseLiteral4,
 
-    for (input) |ch, i| {
-        switch (state) {
-            .Start => {
-                if (ch == '{') {
-                    state = ParserState.ExpectKey;
-                } else {
-                    return ParseError.InvalidStart;
-                }
-            },
-            .ExpectKey => {
-                state = switch (ch) {
-                    '}' => ParserState.Finished,
-                    '\'' => blk: {
-                        start = i + 1;
-                        break :blk ParserState.InKey;
-                    },
-                    else => {
-                        return ParseError.Todo;
-                    },
-                };
-            },
-            .InKey => {
-                state = switch (ch) {
-                    '\'' => blk: {
-                        end = i;
-                        key = input[start..end];
-                        break :blk ParserState.ExpectColon;
-                    },
-                    else => ParserState.InKey, // Waiting for '
-                };
-            },
-            .ExpectColon => {
-                state = switch (ch) {
-                    ':' => blk: {
-                        start = i + 1;
-                        break :blk ParserState.InVal;
-                    },
-                    else => {
-                        return ParseError.ExpectedColon;
-                    },
-                };
-            },
-            .InVal => {
-                state = switch (ch) {
-                    ',' => blk: {
-                        end = i;
-                        const val = input[start..end];
-                        try map.put(key.?, val);
-                        // std.debug.print("put(|{s}|, |{s}|)", .{ key.?, val });
-                        break :blk ParserState.ExpectKey;
-                    },
-                    '}' => blk: {
-                        end = i;
-                        const val = input[start..end];
-                        // std.debug.print("put(|{s}|, |{s}|)", .{ key.?, val });
-                        try map.put(key.?, val);
-                        break :blk ParserState.Finished;
-                    },
-                    else => ParserState.InVal,
-                };
-            },
+        TrueLiteral1,
+        TrueLiteral2,
+        TrueLiteral3,
 
-            else => unreachable,
-        }
-        // std.debug.print("ch: {c}, i: {}, state: {}\n", .{ ch, i, state });
+        Finished,
+    };
+
+    pub const ParseError = error{
+        InvalidStart,
+        ExpectedColon,
+        ExpectedKey,
+        ExpectedTrue,
+        ExpectedFalse,
+    };
+
+    pub const Value = union(enum) {
+        boolean: bool,
+        string: []const u8,
+        tuple: [0:MAX_DIM]u32,
+    };
+
+    state: ParserState = ParserState.Start,
+    start: usize = 0,
+    key: ?[]const u8 = null,
+
+    pub fn init() Parser {
+        return Parser{
+            .state = ParserState.Start,
+        };
     }
 
-    return map;
-}
+    pub fn parseDict(p: *Parser, input: []const u8, allocator: mem.Allocator) anyerror!std.StringHashMap(Value) {
+        var map = std.StringHashMap(Value).init(allocator);
+        errdefer map.deinit();
+
+        for (input) |ch, i| {
+            switch (p.state) {
+                .Start => {
+                    switch (ch) {
+                        '{' => {
+                            p.state = ParserState.ExpectKey;
+                        },
+                        else => {
+                            return ParseError.InvalidStart;
+                        },
+                    }
+                },
+                .ExpectKey => {
+                    switch (ch) {
+                        '}' => {
+                            p.state = ParserState.Finished;
+                        },
+                        ' ', ',' => {}, // Skip whitespace and comma
+                        '\'' => {
+                            p.state = ParserState.InKey;
+                            p.start = i + 1;
+                        },
+                        else => {
+                            return ParseError.ExpectedKey;
+                        },
+                    }
+                },
+                .InKey => {
+                    switch (ch) {
+                        '\'' => {
+                            p.state = ParserState.ExpectColon;
+                            p.key = input[p.start..i];
+                        },
+                        // Waiting for '
+                        else => {
+                            p.state = ParserState.InKey;
+                        },
+                    }
+                },
+                .ExpectColon => {
+                    switch (ch) {
+                        ' ' => {}, // Skip whitespace
+                        ':' => {
+                            p.state = ParserState.ExpectVal;
+                        },
+                        else => {
+                            return ParseError.ExpectedColon;
+                        },
+                    }
+                },
+                .ExpectVal => {
+                    switch (ch) {
+                        ' ' => {}, // Skip whitespace
+                        '\'' => {
+                            p.state = .StringVal;
+                            p.start = i + 1;
+                        },
+                        'T' => {
+                            p.state = .TrueLiteral1;
+                        },
+                        'F' => {
+                            p.state = .FalseLiteral1;
+                        },
+                        else => {
+                            return ParseError.ExpectedColon;
+                        },
+                    }
+                },
+                // True
+                .TrueLiteral1 => { // r -> u
+                    switch (ch) {
+                        'r' => {
+                            p.state = .TrueLiteral2;
+                        },
+                        else => {
+                            return ParseError.ExpectedTrue;
+                        },
+                    }
+                },
+                .TrueLiteral2 => { // u -> e
+                    switch (ch) {
+                        'u' => {
+                            p.state = .TrueLiteral3;
+                        },
+                        else => {
+                            return ParseError.ExpectedTrue;
+                        },
+                    }
+                },
+                .TrueLiteral3 => { // e -> finished
+                    switch (ch) {
+                        'e' => {
+                            p.state = .ExpectKey;
+                            try map.put(p.key.?, Value{ .boolean = true });
+                        },
+                        else => {
+                            return ParseError.ExpectedTrue;
+                        },
+                    }
+                },
+                // False Literal
+                .FalseLiteral1 => { // a -> l
+                    switch (ch) {
+                        'a' => {
+                            p.state = .FalseLiteral2;
+                        },
+                        else => {
+                            return ParseError.ExpectedFalse;
+                        },
+                    }
+                },
+                .FalseLiteral2 => { // l -> s
+                    switch (ch) {
+                        'l' => {
+                            p.state = .FalseLiteral3;
+                        },
+                        else => {
+                            return ParseError.ExpectedTrue;
+                        },
+                    }
+                },
+                .FalseLiteral3 => { // s -> e
+                    switch (ch) {
+                        's' => {
+                            p.state = .FalseLiteral4;
+                        },
+                        else => {
+                            return ParseError.ExpectedFalse;
+                        },
+                    }
+                },
+                .FalseLiteral4 => { // s -> e
+                    switch (ch) {
+                        'e' => {
+                            p.state = .ExpectKey;
+                            try map.put(p.key.?, Value{ .boolean = false });
+                        },
+                        else => {
+                            return ParseError.ExpectedFalse;
+                        },
+                    }
+                },
+                .StringVal => { // s -> e
+                    switch (ch) {
+                        '\'' => {
+                            p.state = .ExpectKey;
+                            try map.put(p.key.?, Value{ .string = input[p.start..i] });
+                        },
+                        else => {},
+                    }
+                },
+                .Finished => {
+                    break;
+                },
+            }
+            // std.debug.print("ch: {c}, i: {}, state: {}\n", .{ ch, i, p.state });
+        }
+
+        return map;
+    }
+};
 
 const testing = std.testing;
 
 test "empty dict" {
     const empty = "{}";
     const alloc = std.testing.allocator;
-    var result = try parseDict(empty[0..], alloc);
+    var parser = Parser.init();
+    var result = try parser.parseDict(empty[0..], alloc);
     defer result.deinit();
     try testing.expectEqual(@intCast(u32, 0), result.count());
 }
 
-test "A string" {
+test "single string" {
     const in = "{'hello':'world'}";
     const alloc = std.testing.allocator;
-    var result = try parseDict(in[0..], alloc);
+    var parser = Parser.init();
+    var result = try parser.parseDict(in[0..], alloc);
     defer result.deinit();
     try testing.expectEqual(@intCast(u32, 1), result.count());
 
     const val = result.get("hello").?;
-    try testing.expect(mem.eql(u8, "'world'", val));
+    const expected = Parser.Value{ .string = "world" };
+    try testing.expectEqualStrings(expected.string, val.string);
+}
+
+test "single true" {
+    const in = "{'ok': True}";
+    const alloc = std.testing.allocator;
+    var parser = Parser.init();
+    var result = try parser.parseDict(in[0..], alloc);
+    defer result.deinit();
+    try testing.expectEqual(@intCast(u32, 1), result.count());
+
+    const val = result.get("ok").?;
+    const expected = Parser.Value{ .boolean = true };
+    try testing.expectEqual(expected, val);
+}
+
+test "single false" {
+    const in = "{'not_ok': False}";
+    const alloc = std.testing.allocator;
+    var parser = Parser.init();
+    var result = try parser.parseDict(in[0..], alloc);
+    defer result.deinit();
+    try testing.expectEqual(@intCast(u32, 1), result.count());
+
+    const val = result.get("not_ok").?;
+    const expected = Parser.Value{ .boolean = false };
+    try testing.expectEqual(expected, val);
+}
+
+test "multiple bools" {
+    const in = "{'not_ok': False, 'is_ok': True}";
+    const alloc = std.testing.allocator;
+    var parser = Parser.init();
+    var result = try parser.parseDict(in[0..], alloc);
+    defer result.deinit();
+    try testing.expectEqual(@intCast(u32, 2), result.count());
+
+    {
+        const val = result.get("not_ok").?;
+        const expected = Parser.Value{ .boolean = false };
+        try testing.expectEqual(expected, val);
+    }
+
+    {
+        const val = result.get("is_ok").?;
+        const expected = Parser.Value{ .boolean = true };
+        try testing.expectEqual(expected, val);
+    }
+}
+
+test "multiple values mixed" {
+    const in = "{'not_ok': False, 'is_ok': True, 'ma_str': 'is_nice'}";
+    const alloc = std.testing.allocator;
+    var parser = Parser.init();
+    var result = try parser.parseDict(in[0..], alloc);
+    defer result.deinit();
+    try testing.expectEqual(@intCast(u32, 3), result.count());
+
+    {
+        const val = result.get("not_ok").?;
+        const expected = Parser.Value{ .boolean = false };
+        try testing.expectEqual(expected, val);
+    }
+
+    {
+        const val = result.get("is_ok").?;
+        const expected = Parser.Value{ .boolean = true };
+        try testing.expectEqual(expected, val);
+    }
+
+    {
+        const val = result.get("ma_str").?;
+        const expected = Parser.Value{ .string = "is_nice" };
+        try testing.expectEqualStrings(expected.string, val.string);
+    }
 }
